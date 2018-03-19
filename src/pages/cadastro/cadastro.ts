c62d93aff8235b0d3aa782bcc0b791c8d80ba44e
KravenRoniel <gustavo5302009@gmail.com>
2018-03-19 20:56:29 -0300

    continuação

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Jogo } from './../../models/jogo';
+import { NavParams, NavController } from 'ionic-angular';
 
 @Component({
     selector: 'cadastro-page',
@@ -9,11 +10,28 @@ import { Jogo } from './../../models/jogo';
 export class Cadastro {
 
     public jogo: Jogo;
+    public onCallback: any;
+    public edicao = false;
 
-    constructor() {
+    constructor(public navParams: NavParams,
+                public navCtrl: NavController) {
         
-        this.jogo = new Jogo();
+        this.onCallback = this.navParams.get('callback');
+        this.jogo = this.navParams.get('jogo');
 
+        // verifica se veio o parametro jogo,
+        // se veio, é porque, é uma alteração
+        if (this.jogo == null){
+            this.jogo = new Jogo();
+        }else{
+            this.edicao = true;
+        }
     }
 
+    salvar(jogoSalvar: Jogo){
+        console.log(jogoSalvar);
+
+        this.onCallback(jogoSalvar);
+        this.navCtrl.pop()
+    }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
+import { Cadastro } from './../cadastro/cadastro';
+import { Jogo } from './../../models/jogo';
 import { Component } from '@angular/core';
 import { NavController, AlertController } from 'ionic-angular';
-import { Cadastro } from '../cadastro/cadastro';
 
 @Component({
   selector: 'page-home',
@@ -8,13 +9,35 @@ import { Cadastro } from '../cadastro/cadastro';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController,
+  public listaJogos: Array<Jogo> = [];
+  
+  constructor(public navCtrl: NavController, 
               public alertCtrl: AlertController) {
-
   }
 
-  abrirCadastro() {
-    this.navCtrl.push(Cadastro)
+  abrirCadastro(item: Jogo) {
+    // Variável "me" criada para referenciar a classe home
+    // dentro da função callback
+    let me = this;
+    // função callback, criada para ser executada no
+    // método de salvar da classe Cadastro
+    let onCallBack = (jogo: Jogo) => {
+      let existe = false;
+      for (let i=0; i < this.listaJogos.length; i++){
+        if (this.listaJogos[i].codigo == jogo.codigo){
+          this.listaJogos[i] = jogo;
+          existe = true;
+        }
+      }
+      if (!existe) me.listaJogos.push(jogo);
+    }
+    // Ao chamar a tela de Cadastro, é passada
+    // a função callback por parâmetro
+    this.navCtrl.push(Cadastro, {jogo: item, callback: onCallBack})
+  }
+
+  apagarItem(posicao: number){
+    this.listaJogos.splice(posicao, 1);
   }
 
 }
